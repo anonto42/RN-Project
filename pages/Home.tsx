a6b2581ae0ec50e4710f1ff72e874482ba4e2f31
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-02-16 17:33:15 +0600

    asd

---
+++ pages/Home.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={{fontSize:12}}>Onim bokachoda</Text>
+    </View>
+  )
+}
+
+export default Home
